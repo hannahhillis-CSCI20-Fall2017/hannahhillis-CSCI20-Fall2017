9ec92f984082b2a0dc68a46cd77934896c80db6e
Hannah Hillis <hhillis001@student.butte.edu>
2017-08-23 19:14:48 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,9 @@
+The numbers available numbers one through ten
+The computer selects a random number in this range 
+The user guesses the number by typing it 
+The computer verifies whenther the guess is right or wrong
+If the user guess is wrong the computer says too high or too low
+The user starts with ten points
+If the guess is wrong one point is subtracted
+Once the user reaches zero they lose
+If the user guesses the number correctly without reaching zero points the user wins
